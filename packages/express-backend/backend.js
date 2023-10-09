ac5e09f4a699560fbef2accc0a342798ee55d542
Andrew Ji <aeji@calpoly.edu>
2023-10-09 16:54:57 -0700

    linked front and back properly(?)

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,58 +36,18 @@ const users = {
 app.use(express.json());
 app.use(cors());
 
+app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+   res.send('Hello World!');
+});
+
 const findUserByName = (name) => { 
    return users['users_list']
        .filter( (user) => user['name'] === name); 
 }
-
-const findUserByNameAndJob = (name, job) => { // don't know if this is correct
-   return users['users_list']
-       .filter( (user) => user['name'] === name , (user) => user['job'] === job);
-}
-
-const findUserById = (id) =>
-    users['users_list']
-        .find( (user) => user['id'] === id);
-
-const addUser = (user) => {
-   users['users_list'].push(user);
-   return user;
-}
-
-app.delete('/users:id', (req, res) => { // this is not working...
-   const id = req.params['id'];
-   let result = findUserById(id);
-   if (result === undefined) {
-       res.status(404).send('Resource not found.');
-   } else {
-      users['users_list'].pop(result);
-   }
-});
-
-app.post('/users', (req, res) => {
-   const userToAdd = req.body;
-   addUser(userToAdd);
-   res.send();
-});
-    
-app.get('/users/:id', (req, res) => {
-    const id = req.params['id']; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined) {
-        res.status(404).send('Resource not found.');
-    } else {
-        res.send(result);
-    }
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
 
 app.get('/users', (req, res) => {
    const name = req.query.name;
@@ -100,6 +60,47 @@ app.get('/users', (req, res) => {
        res.send(users);
    }
 });
+
+const findUserById = (id) => {
+    users['users_list']
+        .find( (user) => user['id'] === id);
+}
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
+
+const addUser = (user) => {
+   users['users_list'].push(user);
+   return user;
+}
+
+app.post('/users', (req, res) => {
+   const userToAdd = req.body;
+   addUser(userToAdd);
+   res.send();
+});
+
+app.delete('/users:id', (req, res) => { // this is not working...
+   const id = req.params['id'];
+   let result = findUserById(id);
+   if (result === undefined) {
+       res.status(404).send('Resource not found.');
+   } else {
+      users['users_list'].pop(result);
+   }
+});
+
+const findUserByNameAndJob = (name, job) => { // don't know if this is correct
+   return users['users_list']
+       .filter( (user) => user['name'] === name , (user) => user['job'] === job);
+}
 
 app.get('/users/:name+:job', (req, res) => { // this is not working... 
    const name = req.query.name;
